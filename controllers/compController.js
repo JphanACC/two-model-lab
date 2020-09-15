0dc9d4fb12290afc2b926bd6d9dab521c0dfbd89
jphan <jphanacc@gmail.com>
2020-09-15 13:58:14 -0700

    commit 3 - added controller and succesfully showed computer index

--- controllers/compController.js
+++ controllers/compController.js
@@ -3,3 +3,11 @@ const router = express.Router();
 const db = require('../models')
 
 //NOTE Create
+
+
+//NOTE Index
+router.get('/', (req, res) => {
+    res.render('computer/index')
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,16 +3,23 @@ const app = express();
 const path = require('path');
 const PORT = 3000;
 const methodOverride = require('method-override');
+app.set('view engine', 'ejs')
+
+//setting up controllers
+const computerController = require('./controllers/compController')
 
 //middleware Stuff
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
+
 app.get('/', (req, res) => {
     res.render('index.ejs');
 })
 
+//Computer route
+app.use("/computerSection", computerController)
 
 
 //listener
