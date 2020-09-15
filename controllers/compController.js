d25e7df228076c9edd7944c50d250f9e47459156
jphan <jphanacc@gmail.com>
2020-09-15 14:24:17 -0700

    commit 4 - working create

--- controllers/compController.js
+++ controllers/compController.js
@@ -2,12 +2,37 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models')
 
-//NOTE Create
+//NOTE Create page route
+router.get('/createListing', (req, res) => {
+    res.render('computer/createListing')
+})
 
+//Create router
+router.post('/', (req, res) => {
+    db.Computer.create(req.body, (err, createNewListing) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(createNewListing);
+            res.redirect('/computerSection');
+        }
+    })
+})
 
 //NOTE Index
 router.get('/', (req, res) => {
-    res.render('computer/index')
+    db.Computer.find({}, (err, showAll) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('computer/index', {
+                computerList: showAll
+            })
+        }
+    })
 })
+
+
+
 
 module.exports = router;
