645b0f54a56104bda01b6c9c85dc5d5dd0bed9a2
jphan <jphanacc@gmail.com>
2020-09-15 13:21:11 -0700

    commit 1 - adding index

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('Express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+const methodOverride = require('method-override');
+
+//middleware Stuff
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+app.get('/index', (req, res) => {
+    res.render('index.ejs');
+})
+
+//listener
+app.listen(PORT, () => {
+    console.log(`Port listening: ${PORT}`);
+
+})
